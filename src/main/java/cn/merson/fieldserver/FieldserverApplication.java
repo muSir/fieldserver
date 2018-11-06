0dd4d0f28e55fd4d73f8b04476485e0c1542d3f6
merson <446463188@qq.com>
2018-11-06 15:56:05 +0800

    create project

---
+++ src/main/java/cn/merson/fieldserver/FieldserverApplication.java
@@ -0,0 +1,12 @@
+package cn.merson.fieldserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldserverApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/merson/fieldserver/FieldserverApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.merson.fieldserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FieldserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
